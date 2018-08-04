646e9fb4d0112163401493de15b4fc96eb9965b4
Kyle Xiong <kylexiong.bear@gmail.com>
2018-08-04 12:22:29 -0400

    Started Collection View with Image and Label in each cell Images for cells are stored in an array

--- KyleEthanCardGame/ViewController.swift
+++ KyleEthanCardGame/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    //this is the array of card images displayed as cards in each cell, images are found in Assets.xcassets
+    let cardImageArray:[String] = ["noCard", "noCard", "noCard", "noCard", "dennisLi", "noCard", "noCard", "noCard", "noCard", "noCard", "noCard", "noCard"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +24,22 @@ class ViewController: UIViewController {
     }
 
 
+    //number of cells
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    //what is in the cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cells = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! myCell
+        
+        //how to scale the image into the square UIimage box - FURTHER RESEARCH NEEDED
+        cells.cardImageView.contentMode = .scaleToFill
+        
+        //every card in the cardImageArray is read and put into each cell
+        cells.cardImageView.image = UIImage(named: cardImageArray[indexPath.row])
+        return cells
+        //maybe try strategy from blog to display image
+    }
 }
 
---
+++ KyleEthanCardGame/myCell.swift
@@ -0,0 +1,14 @@
+//
+//  myCell.swift
+//  KyleEthanCardGame
+//
+//  Created by Kyle on 8/3/18.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class myCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+}
