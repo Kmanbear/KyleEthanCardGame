e0835fc97b31419b06f95720a43be2a0afe113a7
Kyle Xiong <kylexiong.bear@gmail.com>
2018-08-13 15:21:18 -0400

    Added footer and also optimized more flexible code for the drag and drop

---
+++ KyleEthanCardGame/CollectionViewFooter.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewFooter.swift
+//  KyleEthanCardGame
+//
+//  Created by Kyle on 8/12/18.
+//  Copyright © 2018 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewFooter: UICollectionReusableView {
+        
+    @IBOutlet weak var fightingInBetweenLabel: UILabel!
+}
--- KyleEthanCardGame/ViewController.swift
+++ KyleEthanCardGame/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDragDelegate, UICollectionViewDropDelegate {
     
     //this is the array of card images displayed as cards in each cell, images are found in Assets.xcassets
-    var cardImageArray:[String] = ["noCard", "noCard", "noCard", "noCard", "dennisLi", "noCard", "noCard", "noCard", "noCard", "noCard", "noCard", "noCard"]
+    var firstCardImageArray:[String] = ["noCard", "noCard", "noCard", "noCard", "dennisLi"]
+    var firstOpponentCardImageArray:[String] = ["noCard", "dennisLi", "dennisLi", "noCard", "dennisLi", "dennisLi", "dennisLi"]
 
     @IBOutlet weak var firstCardCollectionView: UICollectionView!{
         didSet{
@@ -27,7 +28,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     private func dragItems(at indexPath: IndexPath) -> [UIDragItem]{
         if let draggedCard = (firstCardCollectionView.cellForItem(at: indexPath) as? myCell)?.cardImageView.image{
-            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: draggedCard as NSItemProviderWriting))
+            let dragItem = UIDragItem(itemProvider: NSItemProvider(object: draggedCard as NSItemProviderWriting))//what is NSItemProviderWriting
             dragItem.localObject = draggedCard
             return [dragItem]
         }else{
@@ -41,7 +42,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 //    }
     
     func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
-        return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)//moves card and inserts rather than copies
     }
     
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
@@ -49,23 +50,44 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         for item in coordinator.items {
             if let sourceIndexPath = item.sourceIndexPath{
                 if let draggedCard = item.dragItem.localObject as? NSItemProviderWriting{
-                    collectionView.performBatchUpdates({
-                        //the reason for sourceIndexPath[1] is because sourceIndexPath is an array [section, item], i just care about the item which is the second item of the array
-                        let removedCard = cardImageArray[sourceIndexPath[1]]
-                        cardImageArray.remove(at: sourceIndexPath.item)
-                        cardImageArray.insert(removedCard, at: destinationIndexPath.item)
-                        print(cardImageArray)
-                        firstCardCollectionView.deleteItems(at: [sourceIndexPath])
-                        firstCardCollectionView.insertItems(at: [destinationIndexPath])
-                    })
+                    if sourceIndexPath.section == destinationIndexPath.section {//can't place your own cards in opponent's section
+                        collectionView.performBatchUpdates({
+                            //sourceIndexPath.item returns index
+                            if sourceIndexPath.section == 0{
+                                moveCards(inArray: &firstCardImageArray, using: sourceIndexPath, using: destinationIndexPath)
+                            }else if sourceIndexPath.section == 1{
+                                moveCards(inArray: &firstOpponentCardImageArray, using: sourceIndexPath, using: destinationIndexPath)
+                            }
+                            firstCardCollectionView.deleteItems(at: [sourceIndexPath])//edits the actual view
+                            firstCardCollectionView.insertItems(at: [destinationIndexPath])
+                        })
+                    }
                 }
             }
         }
     }
     
+    func moveCards(inArray array: inout Array<String>, using sourceIndexPath:IndexPath, using destinationIndexPath:IndexPath){
+        let removedCard = array[sourceIndexPath.item]
+        print(sourceIndexPath.item)
+        array.remove(at: sourceIndexPath.item)//edits the array
+        array.insert(removedCard, at: destinationIndexPath.item)
+        print(array)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
     //number of cells
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        var lengthOfArray = -1 //this will not display anything if things fail below
+        if section == 0{
+            lengthOfArray = firstCardImageArray.count
+        } else if section == 1{
+            lengthOfArray = firstOpponentCardImageArray.count
+        }
+        return lengthOfArray
     }
     
     //what is in the cell
@@ -77,10 +99,25 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         cells.cardImageView.contentMode = .scaleToFill
         
         //every card in the cardImageArray is read and put into each cell
-        cells.cardImageView.image = UIImage(named: cardImageArray[indexPath.row])
-        cells.cardLabel.text = "Cool"
+        if indexPath.section == 0 {
+            cells.cardImageView.image = UIImage(named: firstCardImageArray[indexPath.row])
+            cells.cardLabel.text = "Cool"
+        }else if indexPath.section == 1{//second section
+            cells.cardImageView.image = UIImage(named: firstOpponentCardImageArray[indexPath.row])
+            cells.cardLabel.text = "beans"
+        }
+        
         return cells 
         //maybe try strategy from blog to display image
     }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if let collectionViewFooter = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "collectionViewFooter", for: indexPath) as? CollectionViewFooter{
+            collectionViewFooter.fightingInBetweenLabel.text = "Fight!"
+            return collectionViewFooter
+        }
+        return UICollectionReusableView()
+    }
+    
 }
 
